ec8e2083f9c3cf8da1a7e8de7af55827285bb3a1
Svyatoslav <svyatoslavrechkalov@gmail.com>
2024-12-25 19:13:14 +0100

    третий пункт

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+
+const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+module.exports = greetUser;
