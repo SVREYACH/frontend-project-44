7bab09f8e67695639594ed603ea1947c23697e0a
Svyatoslav <svyatoslavrechkalov@gmail.com>
2025-01-15 21:55:53 +0100

    calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import startGame from '../src/index.js';
 import { description, generateQuestionAndAnswer } from '../src/games/calc.js';
-
 startGame(description, generateQuestionAndAnswer);
