76308c57f1a46fef306e4bcc6770fe46ce6f0c04
SVREYACH <157150129+SVREYACH@users.noreply.github.com>
2025-01-17 08:31:23 +0100

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import startGame from '../src/index.js';
 import { description, generateQuestionAndAnswer } from '../src/games/calc.js';
+
 startGame(description, generateQuestionAndAnswer);
